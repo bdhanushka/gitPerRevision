a54d8af8ff9b1c836047637e64fa60257cc4a20f
arperera <arperera@virtusa.com>
2014-10-28 15:32:45 +0530

    add

--- src/com/virtusa/gto/ara/circle/Yellow.java
+++ src/com/virtusa/gto/ara/circle/Yellow.java
@@ -1,14 +1,40 @@
 package com.virtusa.gto.ara.circle;
 
+/**
+ * TODO Class description goes here.
+ * 
+ * @version
+ * @author arperera
+ */
+
 public class Yellow {
 	
 	public void yellow(){
 		System.out.println();
+		 // Default constructor
 	}
+	
+	/*
+     * Cycle Method: Creates a dependency cycle; [let pre = com.virtusa.gto.pb]
+     * [pre].core -> [pre].validate ->
+     */
 	
 	public void yellow01(){
 		System.out.println();
+		   // Writes to a static filed which is incorrect
 	}
+	
+	/**
+     * 
+     * Validates the entry name.
+     * 
+     * Checks if the formatted name is empty or has more characters than
+     * expected
+     * 
+     * @param entryName
+     *            The name needed to be validated
+     * @return true if valid and false otherwise
+     */
 	
 	public void yellow02(int number) {
         System.out.println("Number : " + String.valueOf(number));
@@ -29,5 +55,9 @@ public class Yellow {
                 break;
         }
     }
+	/*
+     * Cycle Method: Creates a dependency cycle; [let pre = com.virtusa.gto.pb]
+     * [pre].core -> [pre].validate ->
+     */
 
 }
