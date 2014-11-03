6a63b28a47e1321d68ada8073c69b89824993666
arperera <arperera@virtusa.com>
2014-11-03 12:16:30 +0530

    add the new package & add test data

---
+++ src/com/virtusa/gto/ara/delete/Calender.java
@@ -0,0 +1,14 @@
+package com.virtusa.gto.ara.delete;
+
+public class Calender {
+	
+	public void calender(){}
+	
+	public void calender01(){
+		System.out.println();
+	}
+	
+	public void calender02(){
+		System.out.println("calender02 is created");
+	}
+}
--- src/com/virtusa/gto/pb/validate/Validator.java
+++ src/com/virtusa/gto/pb/validate/Validator.java
@@ -146,4 +146,24 @@ public class Validator {
                 break;
         }
     }
+    
+    public void copyPasteMethod303(int number) {
+        System.out.println("Number : " + String.valueOf(number));
+        System.err.println("Dummy error");
+        
+        switch (number) {
+            case 1:
+                System.out.println("Case 1");
+                break;
+            case 2:
+                System.out.println("Case 2");
+                break;
+            case 3:
+                System.out.println("Case 3");
+                break;
+            default:
+                System.out.println("Default Case");
+                break;
+        }
+    }
 }
