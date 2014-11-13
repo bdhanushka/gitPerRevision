7c229238b9fc032bf87e5ba4e6dbbb078008e9be
arperera <arperera@virtusa.com>
2014-11-13 14:19:33 +0530

    Changed

--- src/com/virtusa/gto/ara/animal/Kitty.java
+++ src/com/virtusa/gto/ara/animal/Kitty.java
@@ -1,10 +1,30 @@
 package com.virtusa.gto.ara.animal;
 
 
-public class Yellow {
-		public void orange()
+public class Kitty {
+		public void color()
 		{
 		}
+		
+		public void color1(int number) {
+	        System.out.println("Number : " + String.valueOf(number));
+	        System.err.println("Dummy error");
+	        
+	        switch (number) {
+	            case 1:
+	                System.out.println("Case 1");
+	                break;
+	            case 2:
+	                System.out.println("Case 2");
+	                break;
+	            case 3:
+	                System.out.println("Case 3");
+	                break;
+	            default:
+	                System.out.println("Default Case");
+	                break;
+	        }
+	    }
 }
 	
 	
